ec00958a11b62437937474de38fb0100c02742c0
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-10-10 21:38:14 -0300

    Update Main.java

--- pacote/Main.java
+++ pacote/Main.java
@@ -1,11 +1,9 @@
 package pacote;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Funcoes funcoes = new Funcoes();
 
+        funcoes.menu2();
         funcoes.menu1();
-}
+    }
 }
